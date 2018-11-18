69c6ca7152cd39dce385fec1fde372e7a014614c
ajaymonga20 <ajay.monga@ucc.on.ca>
2018-11-18 17:17:06 -0500

    Add files via upload

---
+++ $AM.js
@@ -0,0 +1,167 @@
+//Name: AJAY MONGA
+// Toolz Class
+// Last Update: May 4, 2018
+
+
+function helloWorld() {
+
+    alert("Hello World!")
+}
+
+
+function googleInterviewA(str) {
+
+
+    for (i = 0; i < str.length; i = i + 1)
+
+        for (j = i + 1; j < str.length; j = j + 1) {
+
+
+            if(str[i] == str[j]) {
+
+                return str[i]
+
+
+            }
+
+
+        }
+        return null
+
+
+
+}
+
+function googleinterview1B(str) {
+
+}
+
+function pel(g) {
+
+
+
+}
+
+
+
+function convertBase2(g) {
+
+
+}
+
+
+function convertBase2(g, b){
+
+
+
+}
+
+
+function convertBaseb(n, b) {
+
+    a = n;
+
+    y = "";
+
+    while (a > 0) {
+
+        x = String(a%b)
+
+        a = a/b
+
+        y = y + x
+
+    }
+
+    return y
+
+
+
+}
+
+
+function pel(a) {
+
+    sum = 0;
+
+
+
+
+
+    for (i = 1; i < a ; i = i + 1 ) {
+
+        if (i%3==0) {
+
+            sum = sum + i
+
+        }
+
+        else if (i%5 ==0) {
+
+            sum = sum + i
+
+        }
+
+    }
+
+    return sum
+
+}
+
+
+
+
+function convertBase2(n) {
+
+    a = n;
+
+    y = "";
+
+    while (a > 0) {
+
+        x = String(a%2)
+
+        a = a/2
+
+        y = y + x
+
+    }
+
+    return y0
+
+}
+
+function evenFibonaccinumbners() {
+
+
+    var s = 2;
+    var n1 = 1;
+    var n2 = 2;
+    var nc = 3;
+
+
+    while (nc < n) {
+
+        if (nc % 2 == 0) {
+
+        }
+        return s;
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
